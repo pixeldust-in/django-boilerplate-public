ea4f16ea171016877ddf5e5a64d5c80d4ddd9640
sandiprb <baradiya.sandip@gmail.com>
2023-04-13 13:49:41 +0530

    Minor fixes

--- src/settings.py
+++ src/settings.py
@@ -23,15 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config(
-    "SECRET_KEY",
-)
-PGCRYPTO_KEY = config("PGCRYPTO_KEY", default=None)
+SECRET_KEY = config("SECRET_KEY", default="sample-key")
+PGCRYPTO_KEY = config("PGCRYPTO_KEY", default="sample-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("DEBUG")
+DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=Csv())
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=Csv(), default="localhost")
 
 # Allowed host for X_HOST headers
 ALLOWED_ORIGINAL_HOST = config("ALLOWED_ORIGINAL_HOST", default="")
