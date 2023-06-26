cbe7e6c4598e827a41ba7779e41716dc9d35f665
Sandip B <baradiya.sandip@gmail.com>
2023-06-26 17:28:55 +0530

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -234,8 +234,6 @@ REDIS_PASSWORD = config("REDIS_PASSWORD", default="")
 REDIS_PORT = config("REDIS_PORT", cast=int, default=6379)
 REDIS_CACHE_STORE = config("REDIS_CACHE_STORE", cast=int, default=0)
 REDIS_CONN_STRING = config("REDIS_CONN_STRING", default=f"{REDIS_URL_SCHEME}://:{REDIS_PASSWORD}@{REDIS_HOST}:{REDIS_PORT}/{REDIS_CACHE_STORE}")
-REDIS_CACHE_STORE = config("REDIS_CACHE_STORE", cast=int, default=0)
-
 
 # Redis Cache backend
 CACHES = {
